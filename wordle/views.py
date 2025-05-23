d6ed93f4a38933bd659c1862d96ef31815d175cb
Tachakar <mtoczyski303@gmail.com>
2025-05-23 21:16:43 +0200

    commit

--- wordle/views.py
+++ wordle/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Word
 def main(req):
+    guesses = []
+    for _ in range(6):
+        guesses.append("abcde")
 
-    ctx = {}
+    ctx = {"guesses" : guesses}
     return render(req, "wordle/main.html", ctx)
