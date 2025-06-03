1b23aadbb50859a3311957d6cb18c1522af6c056
Tachakar <mtoczyski303@gmail.com>
2025-06-03 09:42:55 +0200

    started working on popup for victory

---
+++ wordle/ts_src/pop_up.ts
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+	function getLastWord() {
+		const guesses = document.getElementsByClassName('row');
+		for (let i = 0; i < guesses.length; i++) {
+			const row = guesses[i];
+			let rows = Array()
+			for (let j = 0; j < 5; j++) {
+				rows.push(row.children[j].textContent);
+			};
+			if (rows[i] == "     ") {
+				return (rows[i - 1]);
+			};
+		};
+		return null;
+	};
+	const word = getLastWord();
+	if (word !== null) {
+		// IN PROGRESS 
+
+	} else {
+		return
+	};
+
+});
