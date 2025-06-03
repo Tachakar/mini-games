44fd5e485627767dd0f943cccac2c918a950ad35
Tachakar <mtoczyski303@gmail.com>
2025-06-03 08:51:23 +0200

    added working letter checking

--- wordle/views.py
+++ wordle/views.py
@@ -1,7 +1,5 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, request
 import json
-from django.shortcuts import redirect
-from django.urls import reverse
 from django.views.generic import TemplateView
 from .models import Word
 from random import choice
@@ -15,17 +13,15 @@ class WordleView(TemplateView):
 
     def get_random_word(self):
         words = self.model.objects.values_list("text", flat = True)
-        return choice(words)
+        return str(choice(words))
 
     def get(self, request, *args, **kwargs):
-        if 'winning_word' not in request.session:
+        if 'winning_word' not in request.session or request.session['guesses'][5] != "     ":
             request.session['winning_word'] = self.get_random_word()
-        if 'guesses' not in request.session:
-            request.session['guesses'] = ['     ' for _ in range(6)]
-        if request.session['guesses'][5] != "     ":
+        if 'guesses' not in request.session or request.session['guesses'][5] != "     ":
             request.session['guesses'] = ['     ' for _ in range(6)]
         ctx = self.get_context_data()
-        print(request.session.items())
+        print(request.session.get('winning_word'))
         return self.render_to_response(ctx)
 
     def get_context_data(self, **kwargs):
@@ -45,7 +41,4 @@ class WordleView(TemplateView):
         if row_index < 6 and len(guess) == 5:
             guesses[row_index] = guess
             request.session['guesses'] = guesses
-        if row_index >= 6:
-            request.session['guesses'] = ['     ' for _ in range(6)]
-            request.session['winning_word'] = self.get_random_word()
         return JsonResponse({"staus": "ok", "guesses": request.session['guesses']})
