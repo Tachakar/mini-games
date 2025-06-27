adfb0118a668d9b7f37f696c7e8c8d51e5a567ba
Tachakar <mtoczyski303@gmail.com>
2025-06-27 14:30:47 +0200

    fixed styling issues

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.apps import apps
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
@@ -8,7 +8,9 @@ def homepage(req):
     games = []
     for game in apps.get_app_configs():
         curr_game = game.name
-        if curr_game != "home" and curr_game.startswith("django.") == False:
+        if curr_game.startswith('django.') or curr_game.startswith('crispy_') or curr_game == "home":
+            continue
+        else:
             games.append(curr_game)
 
     ctx = {"games":games}
@@ -17,14 +19,21 @@ def homepage(req):
 
 class SignUp(TemplateView):
     template_name = 'home/sign_up.html'
-    success_url = reverse_lazy('homepage')
+    success_url = reverse_lazy('home:homepage')
     def get(self, request):
         form = BaseUserCreationForm
         ctx = {'form':form}
         return self.render_to_response(ctx)
 
     def post(self, request):
-        pass
+        username = request.POST.get('username', False)
+        password1 = request.POST.get('password1', False)
+        password2 = request.POST.get('password2', False)
+        if username == False or password1 == False or password2 == False:
+            #TODO PAGE REFRESH AND TRY AGAIN :)
+            pass
+        print(username,password1,password2)
+        return redirect(self.success_url)
 
 
 class Login(TemplateView):
